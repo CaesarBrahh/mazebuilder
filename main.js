2f6d4f6a7e7719843af48d73b7df213afb1e89d3
Julian Ware <julianware@Julians-MacBook-Air.local>
2026-01-18 22:17:00 -0600

    score added

--- main.js
+++ main.js
@@ -46,6 +46,7 @@ function update() {
     if (snakeX == foodX && snakeY == foodY) {
         addBlock();
         placeFood(); 
+        document.getElementById("score").innerHTML = "Score: " + blocks.length;
     }
 
     context.fillStyle = "lime";
