bcf197cf9dcfa7b3e243dd21175a85e590cd20ff
Julian Ware <julianware@Julians-MacBook-Air.local>
2026-01-18 20:46:37 -0600

    Base Snake Game

--- main.js
+++ main.js
@@ -1,1 +1,83 @@
-console.log("hello nigga")
+// board
+let blockSize = 25;
+let rows = 20;
+let cols = 20;
+let board;
+let context;
+
+// snake head
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+let velocityX = 0;
+let velocityY = 0;
+
+// food
+var foodX;
+var foodY;
+
+// blocks
+blocks = []
+
+window.onload = function() {
+    board = document.getElementById("board")
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); // used for drawing on the board
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    setInterval(update, 1000/10);
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        addBlock();
+        placeFood(); 
+    }
+
+    context.fillStyle = "lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle = "blue";
+    for (block in blocks) {
+        context.fillRect(block[0], block[1], blockSize, blockSize);
+    }
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+}
+
+function placeFood() {
+    // ensure that food isn't placed in position of blocks
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function addBlock() {
+    blockX = foodX;
+    blockY = foodY;
+
+    blocks.push([blockX, blockY]);
+}
